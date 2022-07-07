c03aacc90bed80e86bd0f9c92b372c2295c1a251
JongDo737 <jongmin373@gmail.com>
2022-07-08 04:11:13 +0900

    test

--- app/src/main/java/com/example/whatmain/CalendarFragment.java
+++ app/src/main/java/com/example/whatmain/CalendarFragment.java
@@ -8,57 +8,71 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CalendarFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public class CustomerFragment extends Fragment {
+        EditText editName;
+        EditText editAge;
+        Button editDate;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public CalendarFragment() {
-        // Required empty public constructor
-    }
+        Calendar myCalendar = Calendar.getInstance();
+        //기본값으로 오늘 날짜
+        DatePickerDialog.OnDateSetListener setDate= new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                myCalendar.set(Calendar.YEAR, i);
+                myCalendar.set(Calendar.MONTH,i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH,i2);
+                updateDate();
+            }
+        };
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
-        CalendarFragment fragment = new CalendarFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+        @Override
+        public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                                 Bundle savedInstanceState) {
+            final ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_customer,container,false);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            editName = rootView.findViewById(R.id.editName);
+            editAge = rootView.findViewById(R.id.editAge);
+            editDate = rootView.findViewById(R.id.editDate);
+            Date currentTime = Calendar.getInstance().getTime();
+            editDate.setText(new SimpleDateFormat("YYYY/MM/dd",Locale.getDefault()).format(currentTime));
+
+
+
+            editDate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new DatePickerDialog(getContext(), setDate, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+                }
+            });
+
+            Button button = rootView.findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {    //눌렀을 때 이름이 뜨게
+                    String name = editName.getText().toString();
+                    String age = editAge.getText().toString();
+                    String date = editDate.getText().toString();
+
+                    if(name.length() < 1 | age.length() < 1 | date.length() < 1){
+                        Toast.makeText(getContext(), "정보를 모두 입력하세요", Toast.LENGTH_LONG ).show();
+                    }
+                    else{   //토스트 뜨게
+                        Toast.makeText(getContext(), "이름 : " + name + " 나이 : " + age + " 생년월일 : "+ date, Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+            return rootView;
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        public void updateDate(){   //날짜 형식 변환
+            String format = "YYYY/MM/dd";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format, Locale.KOREA);
+            editDate.setText(simpleDateFormat.format(myCalendar.getTime()));
+        }
     }
 }
--- app/src/main/java/com/example/whatmain/SignUp.java
+++ app/src/main/java/com/example/whatmain/SignUp.java
@@ -2,18 +2,78 @@ package com.example.whatmain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class SignUp extends AppCompatActivity {
     TextView title;
+
+    EditText signUpUsername;
+    EditText signupPW;
+
+    ImageButton calendar;
+    TextView birthText;
+
+    CheckBox checkMale;
+    CheckBox checkFemale;
+
+    Button commitBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        title = (TextView) findViewById(R.id.title);
+        // 밑줄긋기
+        title = findViewById(R.id.title);
         title.setPaintFlags(title.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        signUpUsername =  findViewById(R.id.signUpUsername);
+        signupPW = findViewById(R.id.signupPW);
+        calendar = findViewById(R.id.calendar);
+        birthText = findViewById(R.id.birthText);
+        checkMale = findViewById(R.id.checkMale);
+        checkFemale = findViewById(R.id.checkFemale);
+        commitBtn = findViewById(R.id.commitBtn);
+        Calendar myCalendar = Calendar.getInstance();
+        //기본값으로 오늘 날짜
+        DatePickerDialog.OnDateSetListener setDate= new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                myCalendar.set(Calendar.YEAR, i);
+                myCalendar.set(Calendar.MONTH,i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH,i2);
+                datePicker.updateDate(i,i1,i2);
+            }
+        };
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        commitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserDto userDto = new UserDto();
+                userDto.setUsername(signUpUsername.getText().toString());
+                userDto.setPassword(signupPW.getText().toString());
+                userDto.setBirth(birthText.getText().toString());
+            }
+        });
+
+
+
     }
 }
