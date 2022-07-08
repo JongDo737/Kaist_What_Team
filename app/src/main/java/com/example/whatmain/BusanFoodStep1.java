9a745caba4d00fda0e47017682827b660646cfdf
JongDo737 <jongmin373@gmail.com>
2022-07-08 16:17:30 +0900

    뭐물래 디자인중

---
+++ app/src/main/java/com/example/whatmain/BusanFoodStep1.java
@@ -0,0 +1,14 @@
+package com.example.whatmain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusanFoodStep1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busan_food_step1);
+    }
+}
--- app/src/main/java/com/example/whatmain/BusanRestaurant.java
+++ app/src/main/java/com/example/whatmain/BusanRestaurant.java
@@ -1,5 +1,6 @@
 package com.example.whatmain;
 
+// 공공데이터 받아오는 곳
 public class BusanRestaurant {
 
 }
